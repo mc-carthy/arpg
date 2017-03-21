a02a7e7f370f92042ac29d11d1527e2e0e4f554f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-03-21 16:25:11 +0000

    Create camera follow script

---
+++ Assets/scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	private GameObject player;
+
+    private void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player");
+    }
+
+    private void LateUpdate ()
+    {
+        transform.position = player.transform.position;
+    }
+
+}
