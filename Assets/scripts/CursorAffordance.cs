0dd2c3f9da9d56df3eab1c19bd37a44da722d77f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-03-22 13:18:36 +0000

    Create cursor affordance

---
+++ Assets/scripts/CursorAffordance.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+    [SerializeField]
+    private Texture2D walkCursor = null;
+    [SerializeField]
+    private Texture2D swordCursor = null;
+    [SerializeField]
+    private Texture2D questionCursor = null;
+    [SerializeField]
+    private Vector2 hotSpot = new Vector2 (96f, 96f);
+
+	private CameraRaycaster cameraRaycaster;
+
+    private void Awake ()
+    {
+        cameraRaycaster = GetComponent<CameraRaycaster> ();
+    }
+
+    private void Update ()
+    {
+        switch (cameraRaycaster.LayerHit)
+        {
+            case Layer.Walkable:
+                Cursor.SetCursor (walkCursor, hotSpot, CursorMode.ForceSoftware);
+                break;
+            case Layer.Enemy:
+                Cursor.SetCursor (swordCursor, hotSpot, CursorMode.ForceSoftware);
+                break;
+            default:
+                Cursor.SetCursor (questionCursor, hotSpot, CursorMode.ForceSoftware);
+                return;
+        }
+    }
+
+}
