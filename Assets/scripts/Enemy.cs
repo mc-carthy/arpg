f0796e93907e94bec0feebefd0cff99e892f4be8
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-03-24 13:18:22 +0000

    Add enemy pathfinding

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour {
 
@@ -11,5 +12,27 @@ public class Enemy : MonoBehaviour {
 
     private float maxHealthPoints = 100f;
     private float currentHealthPoints = 100f;
+    private float attackRadius = 5f;
+    private AICharacterControl aiCharacterControl = null;
+    private GameObject player = null;
+
+    private void Awake ()
+    {
+        aiCharacterControl = GetComponent<AICharacterControl> ();
+        player = GameObject.FindGameObjectWithTag ("Player");
+    }
+
+    private void Update ()
+    {
+        float distToPlayerSqr = Vector3.SqrMagnitude (transform.position - player.transform.position);
+        if (distToPlayerSqr <= Mathf.Pow(attackRadius, 2))
+        {
+            aiCharacterControl.SetTarget (player.transform);
+        }
+        else
+        {
+            aiCharacterControl.SetTarget (transform);
+        }
+    }
 
 }
